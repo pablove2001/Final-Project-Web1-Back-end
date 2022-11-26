4764e346584673eb6581bf4575648c55ed90a7ad
Pablo Vergara <vergarapablo2001@gmail.com>
2022-11-25 20:08:56 -0600

    added cors

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 const express = require("express");
 const mongoose = require("mongoose");
+const cors = require("cors");
 
 const apiRoutes = require("./src/routes");
 
@@ -7,7 +8,8 @@ const app = express();
 
 const port = 3000;
 
-//const uri ="mongodb+srv://iteso2022:ITESO1234@cluster0.jqhyj.mongodb.net/agenda?retryWrites=true&w=majority";
+app.use(cors());
+
 const uri =
   "mongodb+srv://api:xog94ZkMFziCdw57@cluster0.b8dmnja.mongodb.net/back-end?retryWrites=true&w=majority";
 
